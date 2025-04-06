d110b298263cd351e65b75d528be20f387f4ac47
suyashsahu00 <suyashsahu00@gmail.com>
2025-04-06 17:32:53 +0530

    Add files via upload

---
+++ Python/Hello World.py
@@ -0,0 +1,24 @@
+"""if 7<6 :
+    print("Hello World")
+elif 5>2:
+    print("Go to hell!")
+else:
+    print("Go to heaven")
+
+x=5
+y="S4"
+print (y)
+
+x=float (5)
+print (x)
+
+fruit ={"Apple":"1","orange","chery"}
+x,y,z=fruit
+def fn1(x,y):
+    print (x+y)
+fn1(5,6)
+"""
+txt="My name is suyash sahu"
+print ("name" in txt)
+x = int (input("enter a number:"))
+print (x)
