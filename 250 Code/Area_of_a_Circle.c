9ae637ffc5e263005f9b828c025f2ab0257441f7
Suyash Sahu <suyashsahu00@gmail.com>
2025-09-25 18:52:45 +0530

    Adding C++ files

--- 250 Code/Area_of_a_Circle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    float r,area;
-    printf("Enter the radius:");
-    scanf("%f",&r);
-    area=3.14*r*r;
-    printf("Area of circle of raduis %f is %f",r,area);
-    return 0;
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-This repository is created for different code in different programming languages.
